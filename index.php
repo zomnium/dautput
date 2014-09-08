cc6c1eb1a68ef74d21f6f73e4b903d80b6454ca2
Tim van Bergenhenegouwen <tim@slurfhondje.nl>
2014-09-08 21:09:07 +0200

    Application startpoint

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Dautput
+ * From data to output.
+ * @author 		Tim van Bergenhenegouwen, Zomnium.
+ * @link 		http://zomnium.com
+ * @copyright 	2014 Zomnium, Tim van Bergenhenegouwen
+ */
+
+// Composer autoloader
+require_once __DIR__.'/vendor/autoload.php';
+
+// Application bootstrap
+$app = null;
